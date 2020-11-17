283f299e408467fa76a193deb4c8bdb471b5a51f
snighdha2010 <snigdha.sahara@gmail.com>
2020-11-17 01:36:47 -0500

    done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='text-white brand text-center pb-3'> 
+        <div className='text-white brand text-center pb-3 footer'> 
             © 2020 All rights reserved | Created by <span ><a className='text-green' href="https://www.linkedin.com/in/snigdha-hassan-8796851b4/">Snigdha Hassan</a></span>.
         </div>
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
 import '../Home/Home.css'
 
 const Header = () => {
-
     return (
       <div className="position-sticky sticky-top header">
         <nav className="navbar m-2 navbar-expand-lg navbar-dark  navbar-fixed-top">
           <NavLink className="navbar-brand" to="/">Snigdha Hassan</NavLink>
             <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
-            <span className="navbar-toggler-icon"></span>
+            <span  className= 'navbar-toggler-icon'></span>
             </button>
           <div className="collapse navbar-collapse " id="navbarMenu">
           <ul  className="navbar-nav ml-auto">
@@ -28,7 +27,7 @@ const Header = () => {
             </li>
             
             <li className="nav-item resume mt-2 ml-2">
-              <a className="nav-link text-dark" href="https://docs.google.com/document/d/1KbyTxxak5an2t8ietlI2QU80KKvgatLToiYVfPVBIv8/edit?fbclid=IwAR1PKanBT1elu2GI0vZfQ3gKAhrP-g0_NQfOHvGcrrkA7xMzRyy6oqBTP2U" rel="noopener noreferrer" target="_blank" >Resume</a>
+              <a className="nav-link text-dark" href="https://docs.google.com/document/d/1GMnLDBIGxsR3dwHraxhxzDe46XUlMNaDNJ7_sVaIEcI/edit?usp=sharing" rel="noopener noreferrer" target="_blank" >Resume</a>
             </li>
           </ul>
           </div>
