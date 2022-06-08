0a85a50aa80b985d863e174627720c6a0322a313
Snigdha Hassan <32148100+snigdha2010@users.noreply.github.com>
2022-06-08 15:41:00 -0400

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='text-white brand text-center pb-3 footer'> 
-            © 2020 All rights reserved | Created by <span ><a className='text-green' href="https://www.linkedin.com/in/snigdha-hassan-8796851b4/">Snigdha Hassan</a></span>.
+            © 2022 All rights reserved | Created by <span ><a className='text-green' href="https://www.linkedin.com/in/snigdha-hassan-8796851b4/">Snigdha Hassan</a></span>.
         </div>
     );
 };
